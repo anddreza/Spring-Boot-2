a8d7b4a25f43eeb85e522bcf00be487e1d39ba85
Andreza <dedezasilva@live.com>
2023-09-22 05:23:37 -0300

    first commit

---
+++ api/src/main/java/br/com/projeto/api/modelo/Pessoa.java
@@ -0,0 +1,46 @@
+package br.com.projeto.api.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//Especificar a criação da tabela
+@Entity
+//Criar o nome da tabela como "pessoas"
+@Table(name = "pessoas")
+public class Pessoa {
+
+	//Responsavel para criação da Primary Key
+	@Id
+	//Informação autoincrementada, toda vez que passar um nome e uma idade será gerada o codigo
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	private String nome;
+	private int idade;
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
